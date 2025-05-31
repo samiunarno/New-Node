481d947a1997849935269b6faaccffa843ea92c1
samiunarno <samiunarno.iub@gmail.com>
2025-05-31 14:43:14 +0600

    change

--- module-2/asynconus.js
+++ module-2/asynconus.js
@@ -1,3 +1,22 @@
 const fs = require("fs");
 
-fs.readFile("");
+console.log("Task1");
+
+let text = "Node JS";
+
+fs.readFileSync("./hello.txt", { encoding: "utf-8" }, (err, data) => {
+  if (err) {
+    console.log("Something went Wrong", err);
+    return;
+  }
+  console.log("After Writing :", data);
+});
+
+fs.readFileSync("./hello.txt", { encoding: "utf-8" }, (err, data) => {
+  if (err) {
+    console.log("Something Went Wrong", err);
+    return;
+  }
+  text = data;
+  console.log("After Reading :", text);
+});
